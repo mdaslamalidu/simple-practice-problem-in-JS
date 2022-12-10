d7cbf456cbc6f92516dad5c81767824525f888b7
mdaslamalidu <thehometutor2022@gmail.com>
2022-12-10 12:56:00 +0600

    add problem

--- assignment.js
+++ assignment.js
@@ -130,4 +130,14 @@ function nameString(name) {
 }
 
 //fifteen
-//
+//Less Than 100?
+function lessThan100(a, b) {
+  let sum = a + b;
+  if (sum < 100) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+console.log(lessThan100(50, 5));
